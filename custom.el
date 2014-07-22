942ed579c1d5377dd388a77f77024b0961244546
Andreas Gerler <baron@bundesbrandschatzamt.de>
2014-07-22 17:31:45 +0200

    custom.el: removed org-agenda-files. They belong into bba.org
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- custom.el
+++ custom.el
@@ -8,7 +8,6 @@
  '(history-length 1000)
  '(magit-commit-signoff t)
  '(magit-repo-dirs (quote ("~/git")))
- '(org-agenda-files (quote ("~/org/emacs.org" "~/org/work.org" "~/org/private.org" "~/org/it.org")))
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(package-user-dir "~/.xemacs/elpa")
  '(recentf-max-saved-items 42)
