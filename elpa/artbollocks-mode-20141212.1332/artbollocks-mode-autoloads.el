66dfbbebad22e24371b129f72fb1884ace88f398
Andreas Gerler <baron@bundesbrandschatzamt.de>
2015-04-28 19:13:00 +0200

    introduced lisp/ directory for cleanup.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/artbollocks-mode-20141212.1332/artbollocks-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; artbollocks-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (artbollocks-mode) "artbollocks-mode" "artbollocks-mode.el"
+;;;;;;  (21823 48772 0 0))
+;;; Generated autoloads from artbollocks-mode.el
+
+(autoload 'artbollocks-mode "artbollocks-mode" "\
+Highlight passive voice, weasel words and artbollocks jargon in text, and provide useful text metrics
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("artbollocks-mode-pkg.el") (21823 48772
+;;;;;;  360275 0))
+
+;;;***
+
+(provide 'artbollocks-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; artbollocks-mode-autoloads.el ends here
---
+++ elpa/visual-regexp-20150329.954/visual-regexp-pkg.el
@@ -0,0 +1,1 @@
+(define-package "visual-regexp" "20150329.954" "A regexp/replace command for Emacs with interactive visual feedback" (quote ((cl-lib "0.2"))))
