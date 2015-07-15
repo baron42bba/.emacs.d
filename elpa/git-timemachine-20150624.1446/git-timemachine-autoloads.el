436d1078c724c305b8682e3bce25787bfc8cfb46
Andreas Gerler <baron@bundesbrandschatzamt.de>
2015-07-15 08:08:54 +0200

    new version of git-timemachine.

--- elpa/git-timemachine-20150624.1446/git-timemachine-autoloads.el
+++ elpa/git-timemachine-20150624.1446/git-timemachine-autoloads.el
@@ -1,11 +1,16 @@
 ;;; git-timemachine-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (git-timemachine) "git-timemachine" "git-timemachine.el"
-;;;;;;  (21593 9364 0 0))
+;;;### (autoloads nil "git-timemachine" "git-timemachine.el" (21908
+;;;;;;  14267 0 0))
 ;;; Generated autoloads from git-timemachine.el
+
+(autoload 'git-timemachine-toggle "git-timemachine" "\
+Toggle git timemachine mode.
+
+\(fn)" t nil)
 
 (autoload 'git-timemachine "git-timemachine" "\
 Enable git timemachine for file of current buffer.
@@ -14,16 +19,9 @@ Enable git timemachine for file of current buffer.
 
 ;;;***
 
-;;;### (autoloads nil nil ("git-timemachine-pkg.el") (21593 9364
-;;;;;;  475894 0))
-
-;;;***
-
-(provide 'git-timemachine-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; git-timemachine-autoloads.el ends here
