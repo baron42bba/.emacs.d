5c8378db0378d3fc2bfd4ac855cd00fd570191a9
Andreas Gerler <baron@bundesbrandschatzamt.de>
2015-12-24 17:38:21 +0100

    adding flymake-puppet and flymake-easy.

---
+++ elpa/flymake-puppet-20141006.1855/flymake-puppet-autoloads.el
@@ -0,0 +1,22 @@
+;;; flymake-puppet-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "flymake-puppet" "flymake-puppet.el" (22030
+;;;;;;  38968 0 0))
+;;; Generated autoloads from flymake-puppet.el
+
+(autoload 'flymake-puppet-load "flymake-puppet" "\
+Configure flymake mode to check the current buffer's puppet syntax.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flymake-puppet-autoloads.el ends here
---
+++ elpa/flymake-puppet-20141006.1855/flymake-puppet-pkg.el
@@ -0,0 +1,1 @@
+(define-package "flymake-puppet" "20141006.1855" "An Emacs flymake handler for syntax-checking puppet using puppet-lint" '((flymake-easy "0.9")) :url "https://github.com/benprew/flymake-puppet")
