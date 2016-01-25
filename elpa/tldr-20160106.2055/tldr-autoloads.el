a572730d9ee3f6453767659306943678afa765ae
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-01-25 20:46:55 +0100

    added tldr
    - Too Long; Didn't Read

---
+++ elpa/tldr-20160106.2055/tldr-autoloads.el
@@ -0,0 +1,26 @@
+;;; tldr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "tldr" "tldr.el" (22164 62880 0 0))
+;;; Generated autoloads from tldr.el
+
+(autoload 'tldr-update-docs "tldr" "\
+Get or update tldr docs from source.
+
+\(fn)" t nil)
+
+(autoload 'tldr "tldr" "\
+Lookup tldr docs.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tldr-autoloads.el ends here
