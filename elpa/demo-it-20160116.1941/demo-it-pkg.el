0ed796f49f3c1cfd41f86f8a5671cd5680db92da
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-01-25 20:46:14 +0100

    added demo-it and org-tree-slide

---
+++ elpa/demo-it-20160116.1941/demo-it-pkg.el
@@ -0,0 +1,1 @@
+(define-package "demo-it" "20160116.1941" "Utility functions for creating demonstrations" 'nil :keywords '("demonstration" "presentation"))
---
+++ elpa/org-tree-slide-20151222.2347/org-tree-slide-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org-tree-slide" "20151222.2347" "A presentation tool for org-mode" 'nil :keywords '("org-mode" "presentation" "narrowing"))
