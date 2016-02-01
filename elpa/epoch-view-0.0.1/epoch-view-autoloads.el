996243405e30b78232a90259208a58bed8393f3d
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-02-01 11:58:52 +0100

    adding epoch-view.
    - converts epoch times to dates via font-lock.

---
+++ elpa/epoch-view-0.0.1/epoch-view-autoloads.el
@@ -0,0 +1,22 @@
+;;; epoch-view-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "epoch-view" "epoch-view.el" (22164 52129 0
+;;;;;;  0))
+;;; Generated autoloads from epoch-view.el
+
+(autoload 'epoch-view-mode "epoch-view" "\
+Visualize epoch (Unix) timestamps.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epoch-view-autoloads.el ends here
---
+++ elpa/epoch-view-0.0.1/epoch-view-pkg.el
@@ -0,0 +1,1 @@
+(define-package "epoch-view" "0.0.1" "Minor mode to visualize epoch timestamps" 'nil :url "http://elpa.gnu.org/packages/epoch-view.html" :keywords '("data" "timestamp" "epoch" "unix"))
