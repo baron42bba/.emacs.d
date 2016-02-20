006674ada6083ef8a43a374e51277be213e6a4f9
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-02-20 13:11:16 +0100

    update bug-hunter.

--- elpa/bug-hunter-0.5/bug-hunter-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "bug-hunter" "0.5" "Hunt down errors in elisp files" '((seq "1.3") (cl-lib "0.5")) :url "http://github.com/Malabarba/elisp-bug-hunter" :keywords '("lisp"))
---
+++ elpa/bug-hunter-1.1/bug-hunter-autoloads.el
@@ -0,0 +1,15 @@
+;;; bug-hunter-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("bug-hunter.el") (22215 9170 116062 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bug-hunter-autoloads.el ends here
