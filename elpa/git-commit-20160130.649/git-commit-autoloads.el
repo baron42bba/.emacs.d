51ae7514964f49ced0e86c38f98eff19bec48bc8
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-02-20 12:49:50 +0100

    update package git-commit.

--- elpa/git-commit-20160130.649/git-commit-autoloads.el
+++ elpa/git-commit-20160130.649/git-commit-autoloads.el
@@ -3,8 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22173 11883 0
-;;;;;;  0))
+;;;### (autoloads nil "git-commit" "git-commit.el" (22206 287 0 0))
 ;;; Generated autoloads from git-commit.el
 
 (defvar global-git-commit-mode t "\
