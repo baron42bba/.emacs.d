d3333c512776e91f08fc939d6deee2488fd811c0
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-02-20 14:14:50 +0100

    update rainbow-mode.

--- elpa/rainbow-mode-0.12/rainbow-mode-autoloads.el
+++ elpa/rainbow-mode-0.12/rainbow-mode-autoloads.el
@@ -1,10 +1,10 @@
 ;;; rainbow-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads (rainbow-mode) "rainbow-mode" "rainbow-mode.el"
-;;;;;;  (21850 7749 0 0))
+;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (22216 26216
+;;;;;;  0 0))
 ;;; Generated autoloads from rainbow-mode.el
 
 (autoload 'rainbow-mode "rainbow-mode" "\
@@ -15,16 +15,14 @@ This will fontify with colors the string like \"#aabbcc\" or \"blue\".
 
 ;;;***
 
-;;;### (autoloads nil nil ("rainbow-mode-pkg.el") (21850 7749 709135
+;;;### (autoloads nil nil ("rainbow-mode-pkg.el") (22216 26216 901245
 ;;;;;;  0))
 
 ;;;***
 
-(provide 'rainbow-mode-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; rainbow-mode-autoloads.el ends here
