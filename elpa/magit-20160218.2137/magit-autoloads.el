6aa19140aa53592f3293ec909ff274cdbeab84a8
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-02-20 14:03:17 +0100

    update magit.

--- elpa/magit-20160218.2137/magit-autoloads.el
+++ elpa/magit-20160218.2137/magit-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-rebase" "git-rebase.el" (22206 293 0 0))
+;;;### (autoloads nil "git-rebase" "git-rebase.el" (22215 7982 0
+;;;;;;  0))
 ;;; Generated autoloads from git-rebase.el
 
 (autoload 'git-rebase-mode "git-rebase" "\
@@ -22,7 +23,7 @@ running 'man git-rebase' at the command line) for details.
 
 ;;;***
 
-;;;### (autoloads nil "magit" "magit.el" (22206 293 0 0))
+;;;### (autoloads nil "magit" "magit.el" (22215 7982 0 0))
 ;;; Generated autoloads from magit.el
 
 (autoload 'magit-status "magit" "\
@@ -455,8 +456,8 @@ Git, and Emacs in the echo area.
 
 ;;;***
 
-;;;### (autoloads nil "magit-apply" "magit-apply.el" (22206 293 0
-;;;;;;  0))
+;;;### (autoloads nil "magit-apply" "magit-apply.el" (22215 7982
+;;;;;;  0 0))
 ;;; Generated autoloads from magit-apply.el
 
 (autoload 'magit-stage-file "magit-apply" "\
@@ -492,8 +493,8 @@ Remove all changes from the staging area.
 
 ;;;***
 
-;;;### (autoloads nil "magit-autorevert" "magit-autorevert.el" (22206
-;;;;;;  293 0 0))
+;;;### (autoloads nil "magit-autorevert" "magit-autorevert.el" (22215
+;;;;;;  7982 0 0))
 ;;; Generated autoloads from magit-autorevert.el
 
 (defvar magit-revert-buffers t)
@@ -521,7 +522,7 @@ See `auto-revert-mode' for more information on Auto-Revert mode.
 
 ;;;***
 
-;;;### (autoloads nil "magit-bisect" "magit-bisect.el" (22206 293
+;;;### (autoloads nil "magit-bisect" "magit-bisect.el" (22215 7982
 ;;;;;;  0 0))
 ;;; Generated autoloads from magit-bisect.el
  (autoload 'magit-bisect-popup "magit-bisect" nil t)
@@ -573,8 +574,8 @@ bisect run'.
 
 ;;;***
 
-;;;### (autoloads nil "magit-blame" "magit-blame.el" (22206 293 0
-;;;;;;  0))
+;;;### (autoloads nil "magit-blame" "magit-blame.el" (22215 7982
+;;;;;;  0 0))
 ;;; Generated autoloads from magit-blame.el
  (autoload 'magit-blame-popup "magit-blame" nil t)
 
@@ -598,7 +599,7 @@ only arguments available from `magit-blame-popup' should be used.
 
 ;;;***
 
-;;;### (autoloads nil "magit-commit" "magit-commit.el" (22206 293
+;;;### (autoloads nil "magit-commit" "magit-commit.el" (22215 7982
 ;;;;;;  0 0))
 ;;; Generated autoloads from magit-commit.el
 
@@ -681,7 +682,8 @@ Create a squash commit targeting COMMIT and instantly rebase.
 
 ;;;***
 
-;;;### (autoloads nil "magit-diff" "magit-diff.el" (22206 293 0 0))
+;;;### (autoloads nil "magit-diff" "magit-diff.el" (22215 7982 0
+;;;;;;  0))
 ;;; Generated autoloads from magit-diff.el
 
 (autoload 'magit-diff-dwim "magit-diff" "\
@@ -692,7 +694,7 @@ Show changes for the thing at point.
 (autoload 'magit-diff "magit-diff" "\
 Show differences between two commits.
 
-REV-OR-RANGE should be a RANGE or a single revision.  If it is a
+REV-OR-RANGE should be a range or a single revision.  If it is a
 revision, then show changes in the working tree relative to that
 revision.  If it is a range, but one side is omitted, then show
 changes relative to `HEAD'.
@@ -746,8 +748,8 @@ for a revision.
 
 ;;;***
 
-;;;### (autoloads nil "magit-ediff" "magit-ediff.el" (22206 293 0
-;;;;;;  0))
+;;;### (autoloads nil "magit-ediff" "magit-ediff.el" (22215 7982
+;;;;;;  0 0))
 ;;; Generated autoloads from magit-ediff.el
  (autoload 'magit-ediff-popup "magit-ediff" nil t)
 
@@ -827,7 +829,7 @@ Show changes introduced by COMMIT using Ediff.
 
 ;;;***
 
-;;;### (autoloads nil "magit-extras" "magit-extras.el" (22206 293
+;;;### (autoloads nil "magit-extras" "magit-extras.el" (22215 7982
 ;;;;;;  0 0))
 ;;; Generated autoloads from magit-extras.el
 
@@ -898,7 +900,7 @@ on a position in a file-visiting buffer.
 
 ;;;***
 
-;;;### (autoloads nil "magit-log" "magit-log.el" (22206 293 0 0))
+;;;### (autoloads nil "magit-log" "magit-log.el" (22215 7982 0 0))
 ;;; Generated autoloads from magit-log.el
 
 (autoload 'magit-log-current "magit-log" "\
@@ -966,7 +968,7 @@ Show commits in a branch that are not merged in the upstream branch.
 
 ;;;***
 
-;;;### (autoloads nil "magit-remote" "magit-remote.el" (22206 293
+;;;### (autoloads nil "magit-remote" "magit-remote.el" (22215 7982
 ;;;;;;  0 0))
 ;;; Generated autoloads from magit-remote.el
 
@@ -1180,8 +1182,8 @@ is asked to pull.  START has to be reachable from that commit.
 
 ;;;***
 
-;;;### (autoloads nil "magit-sequence" "magit-sequence.el" (22206
-;;;;;;  293 0 0))
+;;;### (autoloads nil "magit-sequence" "magit-sequence.el" (22215
+;;;;;;  7982 0 0))
 ;;; Generated autoloads from magit-sequence.el
 
 (autoload 'magit-sequencer-continue "magit-sequence" "\
@@ -1329,8 +1331,8 @@ Abort the current rebase operation, restoring the original branch.
 
 ;;;***
 
-;;;### (autoloads nil "magit-stash" "magit-stash.el" (22206 293 0
-;;;;;;  0))
+;;;### (autoloads nil "magit-stash" "magit-stash.el" (22215 7982
+;;;;;;  0 0))
 ;;; Generated autoloads from magit-stash.el
  (autoload 'magit-stash-popup "magit-stash" nil t)
 
@@ -1429,8 +1431,8 @@ Show all diffs of a stash in a buffer.
 
 ;;;***
 
-;;;### (autoloads nil "magit-submodule" "magit-submodule.el" (22206
-;;;;;;  293 0 0))
+;;;### (autoloads nil "magit-submodule" "magit-submodule.el" (22215
+;;;;;;  7982 0 0))
 ;;; Generated autoloads from magit-submodule.el
  (autoload 'magit-submodule-popup "magit-submodule" nil t)
 
@@ -1493,7 +1495,7 @@ These sections can be expanded to show the respective commits.
 
 ;;;***
 
-;;;### (autoloads nil "magit-wip" "magit-wip.el" (22206 293 0 0))
+;;;### (autoloads nil "magit-wip" "magit-wip.el" (22215 7982 0 0))
 ;;; Generated autoloads from magit-wip.el
 
 (defvar magit-wip-after-save-mode nil "\
@@ -1560,7 +1562,7 @@ command which is about to be called are committed.
 
 ;;;### (autoloads nil nil ("magit-core.el" "magit-git.el" "magit-mode.el"
 ;;;;;;  "magit-pkg.el" "magit-process.el" "magit-section.el" "magit-utils.el")
-;;;;;;  (22206 293 291654 0))
+;;;;;;  (22215 7982 582757 0))
 
 ;;;***
 
