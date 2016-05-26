6d02e3e9827649059084ec675ed4b75d2357294f
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-05-26 20:20:22 +0200

    adding ox-jira
    - a module to convert org-mode into jira syntax.

---
+++ elpa/ox-jira-20160426.553/ox-jira-autoloads.el
@@ -0,0 +1,15 @@
+;;; ox-jira-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ox-jira.el") (22337 64108 292221 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ox-jira-autoloads.el ends here
