41fec0292a19146154f10742c6a0cab075b4d4df
Andreas Gerler <baron@bundesbrandschatzamt.de>
2016-11-28 18:33:07 +0100

    updating org-mode
    - version org-20161118
    - added HP Vertica support to ob-sql
    
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- init.el
+++ init.el
@@ -1,10 +1,11 @@
 (setq load-path
       (append
-       (list (expand-file-name "~/.emacs.d/lisp") (expand-file-name "~/.emacs.d/elpa/org-20160711"))
+       (list (expand-file-name "~/.emacs.d/lisp") (expand-file-name "~/.emacs.d/elpa/org-20161118"))
        load-path))
 
 (setq org-use-extra-keys t)
-(require 'org "~/.emacs.d/elpa/org-20160711/org.el")
+(require 'org "~/.emacs.d/elpa/org-20161118/org.el")
+(require 'ob-core)
 (require 'ob-tangle)
 (require 'ob-sql)
 (org-babel-load-file (expand-file-name "~/.emacs.d/bba.org"))
