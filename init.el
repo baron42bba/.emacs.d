ee903f3feefe6efc358c0e0bbc750dd7a7267347
Andreas Gerler <baron@bundesbrandschatzamt.de>
2017-02-17 21:46:13 +0100

    update org-mode
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- init.el
+++ init.el
@@ -1,10 +1,11 @@
 (setq load-path
       (append
-       (list (expand-file-name "~/.emacs.d/lisp") (expand-file-name "~/.emacs.d/elpa/org-20161118"))
+       (list (expand-file-name "~/.emacs.d/lisp") (expand-file-name "~/.emacs.d/elpa/org-20170210"))
        load-path))
-
+(if (string-equal system-type "darwin")
+    (delete ' "/Applications/Emacs.app/Contents/Resources/lisp/org" load-path))
 (setq org-use-extra-keys t)
-(require 'org "~/.emacs.d/elpa/org-20161118/org.el")
+(require 'org "~/.emacs.d/elpa/org-20170210/org.el")
 (require 'ob-core)
 (require 'ob-tangle)
 (require 'ob-sql)
