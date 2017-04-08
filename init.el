7e6a8e09faaf27cc6fea0171d7667541f4bf7bd2
Andreas Gerler <baron@bundesbrandschatzamt.de>
2017-04-08 20:30:53 +0200

    upgrade org-mode
    - switching to elpa-20170407 created from git repository.
    
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
 (setq load-path
       (append
-       (list (expand-file-name "~/.emacs.d/lisp") (expand-file-name "~/.emacs.d/elpa/org-20170210"))
+       (list (expand-file-name "~/.emacs.d/lisp") (expand-file-name "~/.emacs.d/elpa/org-20170407"))
        load-path))
 (if (string-equal system-type "darwin")
     (delete ' "/Applications/Emacs.app/Contents/Resources/lisp/org" load-path))
 (setq org-use-extra-keys t)
-(require 'org "~/.emacs.d/elpa/org-20170210/org.el")
+(require 'org "~/.emacs.d/elpa/org-20170407/org.el")
 (require 'ob-core)
 (require 'ob-tangle)
 (require 'ob-sql)
