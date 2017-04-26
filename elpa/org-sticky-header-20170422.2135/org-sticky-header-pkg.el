5ad372591777cf1c7356fdf692b223a4818ce455
Andreas Gerler <baron@bundesbrandschatzamt.de>
2017-04-26 13:20:15 +0200

    added org-sticky-header-mode
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/org-sticky-header-20170422.2135/org-sticky-header-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-sticky-header" "20170422.2135" "Show off-screen Org heading at top of window" '((emacs "24.4") (org "8.3.5")) :url "http://github.com/alphapapa/org-sticky-header" :keywords '("hypermedia" "outlines" "org"))
