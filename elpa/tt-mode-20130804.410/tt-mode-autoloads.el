5c4014a9dd41cf544e520173a894e5b70b7d65d0
Andreas Gerler <baron@bundesbrandschatzamt.de>
2017-12-11 15:46:33 +0100

    add tt-mode for Template Toolkit
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/tt-mode-20130804.410/tt-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; tt-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "tt-mode" "tt-mode.el" (23073 12487 679078
+;;;;;;  893000))
+;;; Generated autoloads from tt-mode.el
+
+(autoload 'tt-mode "tt-mode" "\
+Major mode for editing Template Toolkit files
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.tt\\'" . tt-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; tt-mode-autoloads.el ends here
---
+++ elpa/tt-mode-20130804.410/tt-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tt-mode" "20130804.410" "Emacs major mode for editing Template Toolkit files." 'nil :commit "85ed3832e7eef391f7879d9990d59c7a3493c15e")
