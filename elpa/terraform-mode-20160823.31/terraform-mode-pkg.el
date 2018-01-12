12939814a91d8dfc59f51e65e2ff6e0f6afe4b24
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-01-12 16:33:33 +0100

    More cleanup of packages.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/terraform-mode-20160823.31/terraform-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "terraform-mode" "20160823.31" "Major mode for terraform configuration file" '((hcl-mode "0.2") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-terraform-mode")
