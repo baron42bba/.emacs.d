f1202a767a4b9674fa52341a03ee7aee6b0a38c5
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-02-07 15:16:27 +0100

    vertica-snippets update.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/vertica-snippets-20180207.453/vertica-snippets-autoloads.el
+++ elpa/vertica-snippets-20180207.453/vertica-snippets-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "vertica-snippets" "vertica-snippets.el" (23142
-;;;;;;  1522 96098 505000))
+;;;### (autoloads nil "vertica-snippets" "vertica-snippets.el" (23163
+;;;;;;  2511 68580 344000))
 ;;; Generated autoloads from vertica-snippets.el
 
 (autoload 'vertica-snippets-initialize "vertica-snippets" "\
@@ -16,8 +16,8 @@ Add snippet dir to yas-snippet-dirs and load it.
 
 ;;;***
 
-;;;### (autoloads nil nil ("vertica-snippets-pkg.el") (23142 1522
-;;;;;;  93766 839000))
+;;;### (autoloads nil nil ("vertica-snippets-pkg.el") (23163 2511
+;;;;;;  67083 188000))
 
 ;;;***
 
--- elpa/vertica-snippets-20180207.453/vertica-snippets-pkg.el
+++ elpa/vertica-snippets-20180207.453/vertica-snippets-pkg.el
@@ -1,4 +1,4 @@
-(define-package "vertica-snippets" "20180122.44" "Yasnippets for Vertica"
+(define-package "vertica-snippets" "20180207.453" "Yasnippets for Vertica"
   '((yasnippet "0.6.1"))
   :url "https://github.com/baron42bba/vertica-snippets" :keywords
   '("convenience" "snippets"))
