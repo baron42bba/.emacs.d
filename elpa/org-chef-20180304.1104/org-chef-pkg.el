df89a8ebda59c971c96d11dc18441203b00b342b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-03-08 16:56:16 +0100

    adding org-chef
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/org-chef-20180304.1104/org-chef-pkg.el
@@ -0,0 +1,8 @@
+(define-package "org-chef" "20180304.1104" "Cookbook and recipe management with org-mode."
+  '((org "0")
+    (emacs "24"))
+  :url "https://github.com/Chobbes/org-chef" :keywords
+  '("convenience" "abbrev" "outlines" "org" "food" "recipes" "cooking"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
