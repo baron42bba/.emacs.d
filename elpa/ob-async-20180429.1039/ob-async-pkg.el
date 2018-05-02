b4b4f302aaec68d09ca038c2551524a60baf58b9
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-05-02 11:34:07 +0200

    Adding ob-async
    A great package if you do a lot literate database work. No more
    blocking Emacs while waiting for your queries.
    
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/ob-async-20180429.1039/ob-async-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ob-async" "20180429.1039" "Asynchronous org-babel src block execution" '((async "1.9") (org "9.0.1") (emacs "24.4") (dash "2.14.1")) :commit "4bd85c9065f4b075190ffa5a721b9191bc84d6e9" :keywords '("tools") :url "https://github.com/astahlman/ob-async")
