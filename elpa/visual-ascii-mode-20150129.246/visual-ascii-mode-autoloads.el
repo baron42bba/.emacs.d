29c313f17cff3405be5ed6e7b71745c39427139b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-08-28 20:05:17 +0200

    Adding visual-ascii-mode
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/visual-ascii-mode-20150129.246/visual-ascii-mode-autoloads.el
@@ -0,0 +1,30 @@
+;;; visual-ascii-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "visual-ascii-mode" "visual-ascii-mode.el"
+;;;;;;  (23247 3476 722467 459000))
+;;; Generated autoloads from visual-ascii-mode.el
+
+(autoload 'visual-ascii-mode "visual-ascii-mode" "\
+Visualize ascii code on buffer.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'turn-on-visual-ascii-mode "visual-ascii-mode" "\
+Turn on visual-ascii-mode.
+
+\(fn)" nil nil)
+
+(autoload 'global-visual-ascii-mode "visual-ascii-mode" "\
+The globlized visual-ascii-mode" t)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; visual-ascii-mode-autoloads.el ends here
---
+++ elpa/visual-ascii-mode-20150129.246/visual-ascii-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "visual-ascii-mode" "20150129.246" "Visualize ascii code (small integer) on buffer." 'nil :commit "99285a099a17472ddd9f1b4f74e9d092dd8c5947" :keywords '("presentation") :url "https://github.com/Dewdrops/visual-ascii-mode")
