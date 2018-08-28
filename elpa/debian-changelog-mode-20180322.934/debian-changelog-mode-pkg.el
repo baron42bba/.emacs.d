7af95070a62bb70ae01c8844475747ba0dc5a69b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-08-28 20:03:21 +0200

    Adding debian-changelog-mode
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/debian-changelog-mode-20180322.934/debian-changelog-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "debian-changelog-mode" "20180322.934" "major mode for Debian changelog files." 'nil :commit "8ca6eeccb26c71c6166993382917b94b9d3c03e2")
