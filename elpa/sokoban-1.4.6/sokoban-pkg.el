0bdba2d3034560ffa741f02e81db6a659951897f
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-08-28 19:47:03 +0200

    Adding sokoban.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/sokoban-1.4.6/sokoban-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from sokoban.el
+(define-package "sokoban" "1.4.6" "Implementation of Sokoban for Emacs." '((emacs "23.1")) :url "http://elpa.gnu.org/packages/sokoban.html" :keywords '("games"))
