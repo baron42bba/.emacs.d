3fa23b9b6c5bb0c75d6f6767fc17869862ba745d
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-12 17:46:05 +0100

    update apples-mode.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/apples-mode-20110121.418/apples-mode-autoloads.el
+++ elpa/apples-mode-20110121.418/apples-mode-autoloads.el
@@ -1,10 +1,12 @@
 ;;; apples-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "apples-mode" "apples-mode.el" (22813 44928
-;;;;;;  0 0))
+;;;### (autoloads nil "apples-mode" "apples-mode.el" (0 0 0 0))
 ;;; Generated autoloads from apples-mode.el
 
 (autoload 'apples-open-scratch "apples-mode" "\
@@ -17,9 +19,11 @@ Happy AppleScripting!
 
 \(fn)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "apples-mode" '("ac-source-applescript" "apples-")))
+
 ;;;***
 
-;;;### (autoloads nil nil ("apples-mode-pkg.el") (22813 44928 0 0))
+;;;### (autoloads nil nil ("apples-mode-pkg.el") (0 0 0 0))
 
 ;;;***
 
@@ -27,5 +31,6 @@ Happy AppleScripting!
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; apples-mode-autoloads.el ends here
