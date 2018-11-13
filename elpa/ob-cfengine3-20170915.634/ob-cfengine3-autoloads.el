874382bf0b61132b6e94aef90a50f803099e11d6
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 15:06:06 +0100

    update ob-cfengine3.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/ob-cfengine3-20170915.634/ob-cfengine3-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; ob-cfengine3-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("ob-cfengine3.el") (23082 53516 115159
-;;;;;;  478000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ob-cfengine3-autoloads.el ends here
--- elpa/ob-cfengine3-20170915.634/ob-cfengine3-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ob-cfengine3" "20170915.634" "Org Babel functions for CFEngine 3" 'nil :commit "f38f87256efcb2b02f5c7042900087be941c1ddc" :url "https://github.com/nickanderson/ob-cfengine3" :keywords '("tools" "convenience"))
