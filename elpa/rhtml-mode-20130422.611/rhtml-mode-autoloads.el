e10b6d6c2f9b67a9c40ecd9846116bb94121de7e
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 12:08:21 +0100

    update rhtml-mode.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/rhtml-mode-20130422.611/rhtml-mode-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; rhtml-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "rhtml-mode" "rhtml-mode.el" (22228 24358 0
-;;;;;;  0))
-;;; Generated autoloads from rhtml-mode.el
-
-(autoload 'rhtml-mode "rhtml-mode" "\
-Embedded Ruby Mode (RHTML)
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.html\\.erb$" . rhtml-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("rhtml-erb.el" "rhtml-fonts.el" "rhtml-mode-pkg.el"
-;;;;;;  "rhtml-navigation.el" "rhtml-ruby-hook.el" "rhtml-sgml-hacks.el")
-;;;;;;  (22228 24358 363543 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rhtml-mode-autoloads.el ends here
--- elpa/rhtml-mode-20130422.611/rhtml-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "rhtml-mode" "20130422.611" "major mode for editing RHTML files" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
