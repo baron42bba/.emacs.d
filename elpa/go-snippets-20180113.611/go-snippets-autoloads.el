158c6c1ed483e94d852e9b38e1b7c4660edc7cf1
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 11:58:34 +0100

    update go-snippets.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/go-snippets-20180113.611/go-snippets-autoloads.el
+++ elpa/go-snippets-20180113.611/go-snippets-autoloads.el
@@ -1,10 +1,12 @@
 ;;; go-snippets-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "go-snippets" "go-snippets.el" (23126 10835
-;;;;;;  780604 640000))
+;;;### (autoloads nil "go-snippets" "go-snippets.el" (0 0 0 0))
 ;;; Generated autoloads from go-snippets.el
 
 (autoload 'go-snippets-initialize "go-snippets" "\
@@ -14,10 +16,11 @@
 
 (eval-after-load 'yasnippet '(go-snippets-initialize))
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-snippets" '("go-snippets-dir")))
+
 ;;;***
 
-;;;### (autoloads nil nil ("go-snippets-pkg.el") (23126 10835 781783
-;;;;;;  534000))
+;;;### (autoloads nil nil ("go-snippets-pkg.el") (0 0 0 0))
 
 ;;;***
 
@@ -25,5 +28,6 @@
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; go-snippets-autoloads.el ends here
--- elpa/go-snippets-20180113.611/go-snippets-pkg.el
+++ elpa/go-snippets-20180113.611/go-snippets-pkg.el
@@ -1,4 +1,4 @@
-(define-package "go-snippets" "20170831.2302" "Yasnippets for go"
+(define-package "go-snippets" "20180113.611" "Yasnippets for go"
   '((yasnippet "0.8.0")))
 ;; Local Variables:
 ;; no-byte-compile: t
