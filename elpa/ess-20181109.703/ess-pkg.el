e1750b19caa3b80c80fbad3e91b756a91ea163ab
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 14:49:18 +0100

    add ess.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/ess-20181109.703/ess-pkg.el
@@ -0,0 +1,20 @@
+(define-package "ess" "20181109.703" "Emacs Speaks Statistics"
+  '((julia-mode "0.3"))
+  :authors
+  '(("David Smith" . "dsmith@stats.adelaide.edu.au")
+    ("A.J. Rossini" . "blindglobe@gmail.com")
+    ("Richard M. Heiberger" . "rmh@temple.edu")
+    ("Kurt Hornik" . "Kurt.Hornik@R-project.org")
+    ("Martin Maechler" . "maechler@stat.math.ethz.ch")
+    ("Rodney A. Sparapani" . "rsparapa@mcw.edu")
+    ("Stephen Eglen" . "stephen@gnu.org")
+    ("Sebastian P. Luque" . "spluque@gmail.com")
+    ("Henning Redestig" . "henning.red@googlemail.com")
+    ("Vitalie Spinu" . "spinuvit@gmail.com")
+    ("Lionel Henry" . "lionel.hry@gmail.com")
+    ("J. Alexander Branham" . "alex.branham@gmail.com"))
+  :maintainer
+  '("ESS Core Team" . "ESS-core@r-project.org"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
