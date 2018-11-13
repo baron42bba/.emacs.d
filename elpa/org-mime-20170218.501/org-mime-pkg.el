cc1a953f4e0e4f1922dc43a738524dbfbff9e729
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 15:09:10 +0100

    update org-mime.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/org-mime-20170218.501/org-mime-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "org-mime" "20170218.501" "org html export for text/html MIME emails" '((emacs "24") (cl-lib "0.5")) :url "http://github.com/org-mime/org-mime" :keywords '("mime" "mail" "email" "html"))
---
+++ elpa/org-mime-20181023.2314/org-mime-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-mime" "20181023.2314" "org html export for text/html MIME emails" '((emacs "24.4") (cl-lib "0.5")) :commit "1e792ef0616069b3ec74a4b7d96fced8c9c6eb8a" :keywords '("mime" "mail" "email" "html") :authors '(("Eric Schulte")) :maintainer '("Chen Bin (redguardtoo)") :url "http://github.com/org-mime/org-mime")
