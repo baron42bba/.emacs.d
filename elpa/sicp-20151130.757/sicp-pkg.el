4c87eeb6f55066f2660a54a93dee8f90d4d85d66
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 14:40:00 +0100

    update sicp
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/sicp-20151130.757/sicp-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "sicp" "20151130.757" "Structure and Interpretation of Computer Programs in info format" 'nil :url "https://mitpress.mit.edu/sicp")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
