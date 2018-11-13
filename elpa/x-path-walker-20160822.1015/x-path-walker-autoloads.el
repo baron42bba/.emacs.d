775be406280ddf0bf1d57b261c9c2db71930f595
Andreas Gerler <baron@bundesbrandschatzamt.de>
2018-11-13 11:43:26 +0100

    update x-path-walker.
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/x-path-walker-20160822.1015/x-path-walker-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; x-path-walker-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "x-path-walker" "x-path-walker.el" (22477 28061
-;;;;;;  0 0))
-;;; Generated autoloads from x-path-walker.el
-
-(autoload 'helm-x-path-walker "x-path-walker" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("x-path-walker-pkg.el") (22477 28061 263065
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; x-path-walker-autoloads.el ends here
