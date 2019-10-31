120185cc01bf73ffdd567373b557b45ce3fa7d94
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 17:42:37 +0100

    add xah-lookup

---
+++ elpa/xah-lookup-20181225.1942/xah-lookup-autoloads.el
@@ -0,0 +1,41 @@
+;;; xah-lookup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "xah-lookup" "xah-lookup.el" (23994 46373 931146
+;;;;;;  0))
+;;; Generated autoloads from xah-lookup.el
+
+(autoload 'xah-lookup-google "xah-lookup" "\
+Lookup current word or text selection in Google Search.
+Version 2017-02-09
+
+\(fn &optional @WORD)" t nil)
+
+(autoload 'xah-lookup-web "xah-lookup" "\
+Lookup current word or text selection in web search.
+Version 2017-02-09
+
+\(fn &optional @WORD)" t nil)
+
+(autoload 'xah-lookup-wikipedia "xah-lookup" "\
+Lookup current word or text selection in Wikipedia.
+Version 2017-02-09
+
+\(fn &optional @WORD)" t nil)
+
+(autoload 'xah-lookup-word-definition "xah-lookup" "\
+Lookup definition of current word or text selection in URL `http://www.thefreedictionary.com/curlicue'.
+Version 2017-02-09
+
+\(fn &optional @WORD)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; xah-lookup-autoloads.el ends here
---
+++ elpa/xah-lookup-20181225.1942/xah-lookup-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "xah-lookup" "20181225.1942" "look up word on internet." '((emacs "24.1")) :commit "2cafbf3605a8f2ac4c56392c5b1f75adc3b11f24" :keywords '("help" "docs" "convenience") :authors '(("Xah Lee ( http://xahlee.info/ )")) :maintainer '("Xah Lee ( http://xahlee.info/ )") :url "http://ergoemacs.org/emacs/xah-lookup.html")
