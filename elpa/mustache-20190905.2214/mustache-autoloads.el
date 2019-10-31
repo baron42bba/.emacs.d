9b72dbcc76e26c56e8ad964cbbb9a50bf1dbe48f
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 17:04:50 +0100

    update mustache

---
+++ elpa/mustache-20190905.2214/mustache-autoloads.el
@@ -0,0 +1,17 @@
+;;; mustache-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("mustache-lex.el" "mustache-parse.el"
+;;;;;;  "mustache-pkg.el" "mustache-render.el" "mustache.el") (23994
+;;;;;;  46444 106595 624000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; mustache-autoloads.el ends here
--- elpa/mustache-20190905.2214/mustache-pkg.el
+++ elpa/mustache-20190905.2214/mustache-pkg.el
@@ -1,4 +1,4 @@
-(define-package "mustache" "20170923.1233" "a mustache templating library in emacs lisp"
+(define-package "mustache" "20190905.2214" "a mustache templating library in emacs lisp"
   '((ht "0.9")
     (s "1.3.0")
     (dash "1.2.0")))
