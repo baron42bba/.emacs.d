6395157ff0fcaeb8bc5b03ad532fd3a574a9b22b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 18:10:11 +0100

    update jira

---
+++ elpa/ox-jira-20171001.916/ox-jira-autoloads.el
@@ -0,0 +1,15 @@
+;;; ox-jira-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ox-jira.el") (23994 46391 581389 834000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ox-jira-autoloads.el ends here
---
+++ elpa/ox-jira-20171001.916/ox-jira-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ox-jira" "20171001.916" "JIRA Backend for Org Export Engine" '((org "8.3")) :commit "db2ec528f46c9e611624ba28611c440a99bff255" :keywords '("outlines" "hypermedia" "wp") :authors '(("Stig Brautaset" . "stig@brautaset.org")) :maintainer '("Stig Brautaset" . "stig@brautaset.org") :url "https://github.com/stig/ox-jira.el")
