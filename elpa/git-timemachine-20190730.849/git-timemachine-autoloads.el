5c9e8924fddf2bb8803f5fe650eff883fee4f652
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 17:10:51 +0100

    update git-timemachine

--- elpa/git-timemachine-20190730.849/git-timemachine-autoloads.el
+++ elpa/git-timemachine-20190730.849/git-timemachine-autoloads.el
@@ -1,13 +1,10 @@
 ;;; git-timemachine-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "git-timemachine" "git-timemachine.el" (0 0
-;;;;;;  0 0))
+;;;### (autoloads nil "git-timemachine" "git-timemachine.el" (23994
+;;;;;;  46479 777399 176000))
 ;;; Generated autoloads from git-timemachine.el
 
 (autoload 'git-timemachine-toggle "git-timemachine" "\
@@ -25,14 +22,11 @@ Enable git timemachine for current buffer, switching to GIT-BRANCH.
 
 \(fn GIT-BRANCH)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "git-timemachine" '("git-timemachine-")))
-
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; git-timemachine-autoloads.el ends here
