23a90b7585aaad0b6d0340c7a13dee3f9df6481c
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 17:18:57 +0100

    update markdown-mode

--- elpa/markdown-mode-20190802.2215/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20190802.2215/markdown-mode-autoloads.el
@@ -1,12 +1,10 @@
 ;;; markdown-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (0 0 0 0))
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (23994 46457
+;;;;;;  314913 169000))
 ;;; Generated autoloads from markdown-mode.el
 
 (autoload 'markdown-mode "markdown-mode" "\
@@ -38,14 +36,11 @@ Toggle native previewing on save for a specific markdown file.
 
 \(fn &optional ARG)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "markdown-mode" '("gfm-" "markdown" "defun-markdown-")))
-
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
-;; coding: utf-8
 ;; End:
 ;;; markdown-mode-autoloads.el ends here
