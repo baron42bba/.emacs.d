72e6614b92b175fde09ad02f48f831d870fd1a45
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 18:16:05 +0100

    add transient

---
+++ elpa/transient-20191017.1115/transient-autoloads.el
@@ -0,0 +1,16 @@
+;;; transient-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("transient-pkg.el" "transient.el") (23993
+;;;;;;  53925 817643 925000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; transient-autoloads.el ends here
