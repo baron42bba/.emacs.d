ee9c339c834f5dfdb7ed60c83313e527e60c9809
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-10-31 17:39:49 +0100

    cleanup

--- custom.el
+++ custom.el
@@ -4,15 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode t)
- '(custom-safe-themes (quote ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(gnuserv-program (concat exec-directory "/gnuserv"))
  '(history-length 1000)
  '(magit-commit-signoff t)
+ '(magit-git-global-arguments (quote ("--no-pager")))
  '(magit-repo-dirs (quote ("~/git")))
- '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(package-user-dir "~/.emacs.d/elpa")
  '(paradox-github-token t)
- '(recentf-max-saved-items 42)
+ '(safe-local-variable-values (quote ((bba-keep-whitespaces . 1))))
  '(template-default-directories (quote ("~/.emacs.d/templates/")))
  '(toolbar-visible-p nil)
  '(vc-handled-backends (quote (RCS CVS SVN SCCS Bzr Hg Mtn Arch))))
