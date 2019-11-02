b721ef168f26f66dab4cd455eb15b2223ae5618c
Andreas Gerler <baron@bundesbrandschatzamt.de>
2019-11-02 13:52:01 +0100

    add missing dependency packages

---
+++ elpa/julia-mode-20191002.1352/julia-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "julia-mode" "20191002.1352" "Major mode for editing Julia source code" 'nil :keywords
+  '("languages")
+  :url "https://github.com/JuliaLang/julia")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
