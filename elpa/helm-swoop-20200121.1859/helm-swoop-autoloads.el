5d1498816b002c9c58a484c0bdaf7ed0bcfb1940
Andreas Gerler <baron@bundesbrandschatzamt.de>
2020-01-31 17:49:12 +0100

    update helm-swoop

--- elpa/helm-swoop-20200121.1859/helm-swoop-autoloads.el
+++ elpa/helm-swoop-20200121.1859/helm-swoop-autoloads.el
@@ -1,14 +1,17 @@
 ;;; helm-swoop-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "helm-swoop" "helm-swoop.el" (23994 46466 115498
-;;;;;;  456000))
+;;;### (autoloads nil "helm-swoop" "helm-swoop.el" (0 0 0 0))
 ;;; Generated autoloads from helm-swoop.el
 
 (autoload 'helm-swoop-back-to-last-point "helm-swoop" "\
-Go back to last position where `helm-swoop' was called
+Go back to last position where `helm-swoop' was called.
+If $CANSEL is non-nil, store `helm-swoop-last-point'.
 
 \(fn &optional $CANCEL)" t nil)
 
@@ -25,34 +28,36 @@ Invoke `helm-swoop' from isearch.
 \(fn)" t nil)
 
 (autoload 'helm-multi-swoop "helm-swoop" "\
-Usage:
-M-x helm-multi-swoop
-1. Select any buffers by [C-SPC] or [M-SPC]
-2. Press [RET] to start helm-multi-swoop
+Multi swoop for $QUERY in $BUFLIST.
 
-C-u M-x helm-multi-swoop
+Usage:
+  \\[execute-extended-command] helm-multi-swoop
+  1. Select any buffers by [C-SPC] or [M-SPC]
+  2. Press [RET] to start `helm-multi-swoop'
+
+\\[universal-argument] \\[execute-extended-command] helm-multi-swoop
 If you have done helm-multi-swoop before, you can skip select buffers step.
 Last selected buffers will be applied to helm-multi-swoop.
 
 \(fn &optional $QUERY $BUFLIST)" t nil)
 
 (autoload 'helm-multi-swoop-all "helm-swoop" "\
-Apply all buffers to helm-multi-swoop
+Apply all buffers to helm-multi-swoop for $QUERY.
 
 \(fn &optional $QUERY)" t nil)
 
 (autoload 'helm-multi-swoop-org "helm-swoop" "\
-Applies all org-mode buffers to helm-multi-swoop
+Applie all `org-mode' buffers to helm-multi-swoop for $QUERY.
 
 \(fn &optional $QUERY)" t nil)
 
 (autoload 'helm-multi-swoop-current-mode "helm-swoop" "\
-Applies all buffers of the same mode as the current buffer to helm-multi-swoop
+Applie all buffers of the same mode as the current buffer to helm-multi-swoop for $QUERY.
 
 \(fn &optional $QUERY)" t nil)
 
 (autoload 'helm-multi-swoop-projectile "helm-swoop" "\
-Apply all opened buffers of the current project to helm-multi-swoop
+Apply all opened buffers of the current project to helm-multi-swoop for $QUERY.
 
 \(fn &optional $QUERY)" t nil)
 
@@ -67,9 +72,11 @@ Start helm-swoop without pre input query.
 \(fn)" t nil)
 
 (autoload 'helm-multi-swoop-edit "helm-swoop" "\
-
+Multi swoop edit.
 
 \(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-swoop" '("helm-" "get-buffers-matching-mode")))
 
 ;;;***
 
@@ -77,5 +84,6 @@ Start helm-swoop without pre input query.
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; helm-swoop-autoloads.el ends here
