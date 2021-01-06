a59ba1b1fab2f1f8f1b7ce1cedb94b71d172c19c
Andreas Gerler <baron@bundesbrandschatzamt.de>
2021-01-06 16:17:27 +0100

    update async

---
+++ elpa/async-20200809.501/async-pkg.el
@@ -0,0 +1,12 @@
+(define-package "async" "20200809.501" "Asynchronous processing in Emacs"
+  '((emacs "24.3"))
+  :commit "14f48de586b0977e3470f053b810d77b07ea427a" :authors
+  '(("John Wiegley" . "jwiegley@gmail.com"))
+  :maintainer
+  '("John Wiegley" . "jwiegley@gmail.com")
+  :keywords
+  '("convenience" "async")
+  :url "https://github.com/jwiegley/emacs-async")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
