835f632a7c499a0e3ab4da8b2d1a62299eb04b30
Andreas Gerler <baron@bundesbrandschatzamt.de>
2021-02-02 14:56:23 +0100

    update magit

---
+++ elpa/magit-20210128.1702/magit-pkg.el
@@ -0,0 +1,16 @@
+(define-package "magit" "20210128.1702" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "e80eff30d24337a75e07f6d2c154f7d82f140d28" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
