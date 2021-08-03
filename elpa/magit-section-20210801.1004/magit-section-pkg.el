7b6e50ba03ca40efa614e525dc202a9711337cde
Andreas Gerler <baron@bundesbrandschatzamt.de>
2021-08-03 17:56:57 +0200

    update magit

---
+++ elpa/magit-section-20210801.1004/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210801.1004" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210330"))
+  :commit "7b46191dedba053f1c44d5f13d4283860e26cbd6" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
