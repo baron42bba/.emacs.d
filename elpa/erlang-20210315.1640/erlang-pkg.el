8de916b6958834923373691078b6d61229162196
Andreas Gerler <baron@bundesbrandschatzamt.de>
2021-10-16 20:09:44 +0200

    add erlang
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/erlang-20210315.1640/erlang-pkg.el
@@ -0,0 +1,11 @@
+(define-package "erlang" "20210315.1640" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "e782c0305e2fcdff0b9ea9c2b365878e602df04a" :authors
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("erlang" "languages" "processes"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
