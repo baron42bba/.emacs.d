3a621571910cae4335bac7003abe25493e9d68ca
Andreas Gerler <baron@bundesbrandschatzamt.de>
2022-02-11 17:03:33 +0100

    remove cl-lib-0.6.1
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/cl-lib-0.6.1/cl-lib-autoloads.el
+++
@@ -1,23 +0,0 @@
-;;; cl-lib-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "cl-lib" "cl-lib.el" (0 0 0 0))
-;;; Generated autoloads from cl-lib.el
- (let ((d (file-name-directory #$)))
-  (when (member d load-path)
-    (setq load-path (append (remove d load-path) (list d)))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; cl-lib-autoloads.el ends here
--- elpa/cl-lib-0.6.1/cl-lib-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cl-lib" "0.6.1" "Properly prefixed CL functions and macros" 'nil :url "http://elpa.gnu.org/packages/cl-lib.html" :keywords nil)
