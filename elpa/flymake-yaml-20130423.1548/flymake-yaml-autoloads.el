8b46cff45a742d2f15b5570415318d63897e7974
Andreas Gerler <baron@bundesbrandschatzamt.de>
2022-02-11 16:32:18 +0100

    add flymake
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/flymake-yaml-20130423.1548/flymake-yaml-autoloads.el
@@ -0,0 +1,27 @@
+;;; flymake-yaml-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flymake-yaml" "flymake-yaml.el" (0 0 0 0))
+;;; Generated autoloads from flymake-yaml.el
+
+(autoload 'flymake-yaml-load "flymake-yaml" "\
+Configure flymake mode to check the current buffer's YAML syntax.
+
+\(fn)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flymake-yaml" '("flymake-yaml-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flymake-yaml-autoloads.el ends here
---
+++ elpa/flymake-yaml-20130423.1548/flymake-yaml-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flymake-yaml" "20130423.1548" "A flymake handler for YAML" '((flymake-easy "0.1")) :commit "24cb5b744a1796e554e6dbfc6eeb237d06a00b10" :keywords '("yaml") :authors '(("Yasuyuki Oka" . "yasuyk@gmail.com")) :maintainer '("Yasuyuki Oka" . "yasuyk@gmail.com") :url "https://github.com/yasuyk/flymake-yaml")
