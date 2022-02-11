2406c866adb6d8a3e2df4d34808e5016979ad280
Andreas Gerler <baron@bundesbrandschatzamt.de>
2022-02-11 17:10:47 +0100

    add graphviz-mode
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/graphviz-dot-mode-20220117.1537/graphviz-dot-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "graphviz-dot-mode" "20220117.1537" "Mode for the dot-language used by graphviz (att)."
+  '((emacs "25.0"))
+  :commit "a1b7d66f5c20404a1e59c2ee5e841022622535b8" :maintainer
+  '("Pieter Pareit" . "pieter.pareit@gmail.com")
+  :keywords
+  '("mode" "dot" "dot-language" "dotlanguage" "graphviz" "graphs" "att")
+  :url "https://ppareit.github.io/graphviz-dot-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
