46b3190bfaafcd356bb583d4dd982b05730d952d
Andreas Gerler <baron@bundesbrandschatzamt.de>
2022-04-19 10:40:05 +0200

    upgrade cider
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/cider-20220417.951/cider-pkg.el
@@ -0,0 +1,23 @@
+(define-package "cider" "20220417.951" "Clojure Interactive Development Environment that Rocks"
+  '((emacs "26")
+    (clojure-mode "5.14")
+    (parseedn "1.0.6")
+    (queue "0.2")
+    (spinner "1.7")
+    (seq "2.22")
+    (sesman "0.3.2"))
+  :commit "9130c6493f339edf3aa24da928d1f1c4edef6582" :authors
+  '(("Tim King" . "kingtim@gmail.com")
+    ("Phil Hagelberg" . "technomancy@gmail.com")
+    ("Bozhidar Batsov" . "bozhidar@batsov.dev")
+    ("Artur Malabarba" . "bruce.connor.am@gmail.com")
+    ("Hugo Duncan" . "hugo@hugoduncan.org")
+    ("Steve Purcell" . "steve@sanityinc.com"))
+  :maintainer
+  '("Bozhidar Batsov" . "bozhidar@batsov.dev")
+  :keywords
+  '("languages" "clojure" "cider")
+  :url "http://www.github.com/clojure-emacs/cider")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
