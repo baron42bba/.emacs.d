4342c6c7ff704e0cf198ff66972bbc87849edfad
Andreas Gerler <baron@bundesbrandschatzamt.de>
2022-12-11 13:23:55 +0100

    update magit and magit-section
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/magit-20220518.2359/magit-pkg.el
@@ -0,0 +1,19 @@
+(define-package "magit" "20220518.2359" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (compat "28.1.1.0")
+    (dash "20210826")
+    (git-commit "20220222")
+    (magit-section "20220325")
+    (transient "20220325")
+    (with-editor "20220318"))
+  :commit "e920cdb326d2c2da5a32809085aa6b9369b51b1a" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-section-20220513.1429/magit-section-pkg.el
@@ -0,0 +1,14 @@
+(define-package "magit-section" "20220513.1429" "Sections for read-only buffers."
+  '((emacs "25.1")
+    (compat "28.1.1.0")
+    (dash "20210826"))
+  :commit "e920cdb326d2c2da5a32809085aa6b9369b51b1a" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
