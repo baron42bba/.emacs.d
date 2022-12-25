1fc25938f0af89dcc7e1ea51dde9f62f5ce9e70b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2022-12-25 15:14:14 +0100

    add go-translate
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/go-translate-20221211.1327/go-translate-pkg.el
@@ -0,0 +1,12 @@
+(define-package "go-translate" "20221211.1327" "Translation framework supports multiple engines such as Google/Bing/DeepL"
+  '((emacs "27.1"))
+  :commit "65d5139f202320b71e6b8b03d752d02b7c0b8c6f" :authors
+  '(("lorniu" . "lorniu@gmail.com"))
+  :maintainer
+  '("lorniu" . "lorniu@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/lorniu/go-translate")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
