032fdb666cbffb29438bd81dc5d2ac2dd28b436a
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-05-11 17:39:37 +0200

    add org-ref and citeproc
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/org-ref-20230507.1420/org-ref-pkg.el
@@ -0,0 +1,24 @@
+(define-package "org-ref" "20230507.1420" "citations, cross-references and bibliographies in org-mode"
+  '((org "9.4")
+    (dash "0")
+    (s "0")
+    (f "0")
+    (htmlize "0")
+    (hydra "0")
+    (avy "0")
+    (parsebib "0")
+    (bibtex-completion "0")
+    (citeproc "0")
+    (ox-pandoc "0"))
+  :commit "83396bfa793850e8bc43755912bbdede0df87815" :authors
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainers
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainer
+  '("John Kitchin" . "jkitchin@andrew.cmu.edu")
+  :keywords
+  '("org-mode" "cite" "ref" "label")
+  :url "https://github.com/jkitchin/org-ref")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
