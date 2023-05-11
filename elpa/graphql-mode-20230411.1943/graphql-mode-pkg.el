684c0a9820bd1cd844d1269eda02fce7cefc684d
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-05-11 18:10:13 +0200

    add graphql graphql-mode ob-graphql
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/graphql-mode-20230411.1943/graphql-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from graphql-mode.el  -*- no-byte-compile: t -*-
+(define-package "graphql-mode" "20230411.1943" "Major mode for editing GraphQL schemas" '((emacs "24.3")) :commit "0ad4ae0160cabad46109dfbb394297a185eb2525" :authors '(("David Vazquez Pua" . "davazp@gmail.com")) :maintainers '(("David Vazquez Pua" . "davazp@gmail.com")) :maintainer '("David Vazquez Pua" . "davazp@gmail.com") :keywords '("languages") :url "https://github.com/davazp/graphql-mode")
