432aae7f443438927129166bc9b89fb1714f937d
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-11-23 19:52:33 +0100

    update ejc-sql
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/ejc-sql-20230702.2048/ejc-sql-pkg.el
+++ elpa/ejc-sql-20230702.2048/ejc-sql-pkg.el
@@ -1,9 +1,9 @@
-(define-package "ejc-sql" "20230228.102" "Emacs SQL client uses Clojure JDBC."
+(define-package "ejc-sql" "20230702.2048" "Emacs SQL client uses Clojure JDBC."
   '((emacs "26.3")
     (clomacs "0.0.5")
     (dash "2.16.0")
     (spinner "1.7.3"))
-  :commit "29308faad38e9cabd98fb5a8450df15db1e4a4cb" :authors
+  :commit "cfb2a3b00d16c6b8575b84d4963ac0c21f91fa05" :authors
   '(("Kostafey" . "kostafey@gmail.com"))
   :maintainers
   '(("Kostafey" . "kostafey@gmail.com"))
