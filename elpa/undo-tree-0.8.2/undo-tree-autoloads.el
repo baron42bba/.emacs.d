47bba33c1047acf2698216d579f145b49247a215
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-11-23 20:12:15 +0100

    update undo-tree
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/undo-tree-0.8.2/undo-tree-autoloads.el
+++ elpa/undo-tree-0.8.2/undo-tree-autoloads.el
@@ -1,4 +1,4 @@
-;;; undo-tree-autoloads.el --- automatically extracted autoloads
+;;; undo-tree-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -15,6 +15,20 @@ With no argument, this command toggles the mode.
 A positive prefix argument turns the mode on.
 A negative prefix argument turns it off.
 
+This is a minor mode.  If called interactively, toggle the
+`Undo-Tree mode' mode.  If the prefix argument is positive,
+enable the mode, and if it is zero or negative, disable the mode.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `undo-tree-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
+
 Undo-tree-mode replaces Emacs' standard undo feature with a more
 powerful yet easier to use version, that treats the undo history
 as what it is: a tree.
@@ -29,6 +43,8 @@ Within the undo-tree visualizer, the following keys are available:
 
 \(fn &optional ARG)" t nil)
 
+(put 'global-undo-tree-mode 'globalized-minor-mode t)
+
 (defvar global-undo-tree-mode nil "\
 Non-nil if Global Undo-Tree mode is enabled.
 See the `global-undo-tree-mode' command
@@ -42,16 +58,24 @@ or call the function `global-undo-tree-mode'.")
 (autoload 'global-undo-tree-mode "undo-tree" "\
 Toggle Undo-Tree mode in all buffers.
 With prefix ARG, enable Global Undo-Tree mode if ARG is positive;
-otherwise, disable it.  If called from Lisp, enable the mode if
-ARG is omitted or nil.
+otherwise, disable it.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.
+Enable the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
 
 Undo-Tree mode is enabled in all buffers where
 `turn-on-undo-tree-mode' would do it.
+
 See `undo-tree-mode' for more information on Undo-Tree mode.
 
 \(fn &optional ARG)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "undo-tree" '("undo-" "turn-on-undo-tree-mode" "*undo-tree-id-counter*" "buffer-undo-tree")))
+(register-definition-prefixes "undo-tree" '("*undo-tree-id-counter*" "buffer-undo-tree" "turn-on-undo-tree-mode" "undo-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("undo-tree-pkg.el") (0 0 0 0))
 
 ;;;***
 
