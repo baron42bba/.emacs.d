a8079c7c0b8ffef0b6f61a790600285ab19c0e95
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-11-25 11:42:31 +0100

    update mustache
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/mustache-20230713.514/mustache-pkg.el
@@ -0,0 +1,16 @@
+(define-package "mustache" "20230713.514" "Mustache templating library in emacs lisp"
+  '((emacs "26")
+    (s "1.3.0")
+    (dash "1.2.0"))
+  :commit "229e01f0f0a5684499bcc6a11a5bf8dbe14fd4e8" :authors
+  '(("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainers
+  '(("Wilfred Hughes" . "me@wilfred.me.uk"))
+  :maintainer
+  '("Wilfred Hughes" . "me@wilfred.me.uk")
+  :keywords
+  '("convenience" "mustache" "template")
+  :url "https://github.com/Wilfred/mustache.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
