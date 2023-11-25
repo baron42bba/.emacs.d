c69b070c5104e5319735ff667f09e7e4a58d1237
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-11-25 11:38:12 +0100

    update htmlize
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/htmlize-20210825.2150/htmlize-autoloads.el
+++ elpa/htmlize-20210825.2150/htmlize-autoloads.el
@@ -1,4 +1,4 @@
-;;; htmlize-autoloads.el --- automatically extracted autoloads
+;;; htmlize-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
 ;;
 ;;; Code:
 
@@ -69,7 +69,11 @@ HTMLize dired-marked files.
 
 \(fn ARG &optional TARGET-DIRECTORY)" t nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "htmlize" '("htmlize-")))
+(register-definition-prefixes "htmlize" '("htmlize-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("htmlize-pkg.el") (0 0 0 0))
 
 ;;;***
 
