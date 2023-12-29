f9ed8d8d5e0f4887d4d17b04736e756dfb702f12
Andreas Gerler <baron@bundesbrandschatzamt.de>
2023-12-29 11:22:49 +0100

    update simple-httpd
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

--- elpa/simple-httpd-20230821.1458/simple-httpd-pkg.el
+++ elpa/simple-httpd-20230821.1458/simple-httpd-pkg.el
@@ -1,6 +1,6 @@
-(define-package "simple-httpd" "20230619.642" "pure elisp HTTP server"
+(define-package "simple-httpd" "20230821.1458" "pure elisp HTTP server"
   '((cl-lib "0.3"))
-  :commit "6260232da8b53c4e214bc5e427ec00c3ebdd9869" :authors
+  :commit "347c30494d3bcfc79de35e54538f92f4e4a46ecd" :authors
   '(("Christopher Wellons" . "wellons@nullprogram.com"))
   :maintainers
   '(("Christopher Wellons" . "wellons@nullprogram.com"))
