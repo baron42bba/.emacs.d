935d424466bf685abec1bfe698844d337a5a8bd4
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-01-29 10:27:22 +0100

    add pinentry and use ash instead of lsh
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/pinentry-0.1/pinentry-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from pinentry.el  -*- no-byte-compile: t -*-
+(define-package "pinentry" "0.1" "GnuPG Pinentry server implementation" 'nil :url "http://elpa.gnu.org/packages/pinentry.html" :keywords '("gnupg") :authors '(("Daiki Ueno" . "ueno@gnu.org")) :maintainer '("Daiki Ueno" . "ueno@gnu.org"))
