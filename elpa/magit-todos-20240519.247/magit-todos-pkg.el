ecb7d72593a2373b244048f233554284e1ef6feb
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-05-28 10:25:18 +0200

    update magit and friends
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/magit-todos-20240519.247/magit-todos-pkg.el
@@ -0,0 +1,22 @@
+(define-package "magit-todos" "20240519.247" "Show source file TODOs in Magit"
+  '((emacs "26.1")
+    (async "1.9.2")
+    (dash "2.13.0")
+    (f "0.17.2")
+    (hl-todo "1.9.0")
+    (magit "2.13.0")
+    (pcre2el "1.8")
+    (s "1.12.0")
+    (transient "0.2.0"))
+  :commit "501c8db90ab59f8b619618b9d10db2a32a113727" :authors
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainers
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainer
+  '("Adam Porter" . "adam@alphapapa.net")
+  :keywords
+  '("magit" "vc")
+  :url "http://github.com/alphapapa/magit-todos")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
