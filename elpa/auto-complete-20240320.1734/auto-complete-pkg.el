1f31795dab3079adfc6ac6da49a39c4bbe9712be
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-21 17:05:50 +0100

    update auto-comlete
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/auto-complete-20240320.1734/auto-complete-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "auto-complete" "20240320.1734"
+  "Auto Completion for GNU Emacs."
+  '((popup  "0.5.0")
+    (cl-lib "0.5"))
+  :url "https://github.com/auto-complete/auto-complete"
+  :commit "0c2f5a7d28b70bfe30b87378d58d74798a62741d"
+  :revdesc "0c2f5a7d28b7"
+  :keywords '("completion" "convenience")
+  :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com"))
+  :maintainers '(("Jen-Chieh Shen" . "jcs090218@gmail.com")))
