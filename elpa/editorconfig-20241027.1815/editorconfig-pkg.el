23903eb63ccfb34ba595b143a19962f9c16224fa
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-21 17:42:57 +0100

    update editorconfig
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/editorconfig-20241027.1815/editorconfig-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "editorconfig" "20241027.1815"
+  "EditorConfig Emacs Plugin."
+  '((emacs "26.1"))
+  :url "https://github.com/editorconfig/editorconfig-emacs"
+  :commit "24f5b2b1cd4e37adc245fb59f7edeb6872a707a4"
+  :revdesc "24f5b2b1cd4e"
+  :keywords '("convenience" "editorconfig")
+  :authors '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
+  :maintainers '(("EditorConfig Team" . "editorconfig@googlegroups.com")))
