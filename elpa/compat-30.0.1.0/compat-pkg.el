82c87ce91b9f28338bd868046a4eb8996e980da8
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-21 17:36:54 +0100

    update compat
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/compat-30.0.1.0/compat-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from compat.el  -*- no-byte-compile: t -*-
+(define-package "compat" "30.0.1.0" "Emacs Lisp Compatibility Library" '((emacs "24.4") (seq "2.23")) :commit "3626216e7665bfe20f41f9b3d6bbeb1a7de2b1a6" :authors '(("Philip Kaludercic" . "philipk@posteo.net") ("Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Compat Development" . "~pkal/compat-devel@lists.sr.ht") :keywords '("lisp" "maint") :url "https://github.com/emacs-compat/compat")
