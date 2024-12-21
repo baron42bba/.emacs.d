9dee38b55b95950d93eeb2f12449d8da08a0d58a
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-21 17:34:53 +0100

    update company
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/company-20241210.2019/company-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "company" "20241210.2019"
+  "Modular text completion framework."
+  '((emacs "26.1"))
+  :url "https://github.com/company-mode/company-mode"
+  :commit "7805174a9ffae3194d5bbde16febcf26761f0ad7"
+  :revdesc "7805174a9ffa"
+  :keywords '("abbrev" "convenience" "matching")
+  :maintainers '(("Dmitry Gutov" . "dmitry@gutov.dev")))
