7b3041429600af0c3bad45021ee90fc9b3eb583e
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-22 13:49:13 +0100

    add tp
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/tp-20241031.729/tp-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "tp" "20241031.729"
+  "Utilities for transient menus that POST to an API."
+  '((emacs     "28.1")
+    (transient "0.5.0"))
+  :url "https://codeberg.org/martianh/tp.el"
+  :commit "df6490d86f24fff22f5ea4f7d887fc60caed1163"
+  :revdesc "df6490d86f24"
+  :keywords '("convenience" "api" "requests")
+  :authors '(("Marty Hiatt" . "mousebot@disroot.org"))
+  :maintainers '(("Marty Hiatt" . "mousebot@disroot.org")))
