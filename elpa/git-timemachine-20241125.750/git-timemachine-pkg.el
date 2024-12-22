1ee247050ad80c29f00761c9b0771c6a30ea7c0b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-22 13:01:10 +0100

    update git-timemachine
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/git-timemachine-20241125.750/git-timemachine-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "git-timemachine" "20241125.750"
+  "Walk through git revisions of a file."
+  '((emacs     "24.3")
+    (transient "0.1.0"))
+  :url "https://codeberg.org/pidu/git-timemachine"
+  :commit "62ebb0c6588d848b1b7b90bf1a3aa4c04e873822"
+  :revdesc "62ebb0c6588d"
+  :keywords '("vc")
+  :authors '(("Peter Stiernström" . "peter@stiernstrom.se"))
+  :maintainers '(("Peter Stiernström" . "peter@stiernstrom.se")))
