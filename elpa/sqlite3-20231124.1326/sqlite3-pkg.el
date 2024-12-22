4c24cd3925287403d53ffd8e548d23e5439c82da
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-22 13:19:06 +0100

    update sqlite3
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/sqlite3-20231124.1326/sqlite3-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "sqlite3" "20231124.1326"
+  "Direct access to the core SQLite3 API."
+  '((emacs "25.1"))
+  :url "https://github.com/pekingduck/emacs-sqlite3-api"
+  :commit "a601c9965e4d0178705a64b7d4f88709ca9aea66"
+  :revdesc "a601c9965e4d"
+  :keywords '("comm" "data" "sql")
+  :authors '(("Y. N. Lo" . "elisp@fastmail.com"))
+  :maintainers '(("Y. N. Lo" . "elisp@fastmail.com")))
