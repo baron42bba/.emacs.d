f55b7c8d56950dc02e8e450538265a29e6814d91
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-22 13:12:10 +0100

    update org-tree-slide
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/org-tree-slide-20230826.1234/org-tree-slide-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "org-tree-slide" "20230826.1234"
+  "A presentation tool for org-mode."
+  '((emacs "25.2"))
+  :url "https://github.com/takaxp/org-tree-slide"
+  :commit "941e7e6cb8a5a0b193345263ed912cceecae1887"
+  :revdesc "941e7e6cb8a5"
+  :keywords '("convenience" "org-mode" "presentation" "narrowing")
+  :authors '(("Takaaki ISHIKAWA" . "takaxpatieeedotorg"))
+  :maintainers '(("Takaaki ISHIKAWA" . "takaxpatieeedotorg")))
