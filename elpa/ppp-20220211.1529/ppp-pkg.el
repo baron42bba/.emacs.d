f28913924c5a6eebf61fe93a60c9adcbfc53f3d9
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-22 13:48:20 +0100

    add ppp
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/ppp-20220211.1529/ppp-pkg.el
@@ -0,0 +1,14 @@
+(define-package "ppp" "20220211.1529" "Extended pretty printer for Emacs Lisp"
+  '((emacs "25.1"))
+  :commit "d5d854c3006dfd268e62c7f91c2aad6f86a505b5" :authors
+  '(("Naoya Yamashita" . "conao3@gmail.com"))
+  :maintainers
+  '(("Naoya Yamashita" . "conao3@gmail.com"))
+  :maintainer
+  '("Naoya Yamashita" . "conao3@gmail.com")
+  :keywords
+  '("tools")
+  :url "https://github.com/conao3/ppp.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
