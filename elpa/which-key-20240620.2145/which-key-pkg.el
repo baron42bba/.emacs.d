3f9ab87d6d8c429b2eae8d09530fe5151e78c233
Andreas Gerler <baron@bundesbrandschatzamt.de>
2024-12-22 13:23:36 +0100

    update which-key
    Signed-off-by: Andreas Gerler <baron@bundesbrandschatzamt.de>

---
+++ elpa/which-key-20240620.2145/which-key-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "which-key" "20240620.2145"
+  "Display available keybindings in popup."
+  '((emacs "25.1"))
+  :url "https://github.com/justbur/emacs-which-key"
+  :commit "ed389312170df955aaf10c2e120cc533ed5c509e"
+  :revdesc "ed389312170d"
+  :authors '(("Justin Burkett" . "justin@burkett.cc"))
+  :maintainers '(("Justin Burkett" . "justin@burkett.cc")))
