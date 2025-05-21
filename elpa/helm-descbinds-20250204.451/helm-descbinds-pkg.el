cf6e1d8fe514c485d2e1b1b4f82fce84a5a71f57
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-05-21 14:44:58 +0200

    update helm

---
+++ elpa/helm-descbinds-20250204.451/helm-descbinds-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm-descbinds" "20250204.451"
+  "A convenient `describe-bindings' with `helm'."
+  '((helm "1.5"))
+  :url "https://github.com/emacs-helm/helm-descbinds"
+  :commit "c12bc85ef3ce342fe1c78cdd86117c05d5310789"
+  :revdesc "c12bc85ef3ce"
+  :keywords '("helm" "help")
+  :authors '(("Taiki SUGAWARA" . "buzz.taiki@gmail.com"))
+  :maintainers '(("Taiki SUGAWARA" . "buzz.taiki@gmail.com")))
---
+++ elpa/helm-projectile-20250218.1525/helm-projectile-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm-projectile" "20250218.1525"
+  "Helm integration for Projectile."
+  '((helm       "3.0")
+    (projectile "2.9")
+    (cl-lib     "0.3"))
+  :url "https://github.com/bbatsov/helm-projectile"
+  :commit "041076e35a6663302a91a0fa672f847c7d64bf29"
+  :revdesc "041076e35a66"
+  :keywords '("project" "convenience"))
