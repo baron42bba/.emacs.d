d2bd7a93ba90d0ebe556794aacf812a0c5025bbc
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-05-21 14:54:39 +0200

    update package-lint

--- elpa/package-lint-20250418.1425/package-lint-pkg.el
+++ elpa/package-lint-20250418.1425/package-lint-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "package-lint" "20241127.1826"
+(define-package "package-lint" "20250418.1425"
   "A linting library for elisp package authors."
   '((emacs     "24.4")
     (let-alist "1.0.6"))
   :url "https://github.com/purcell/package-lint"
-  :commit "0dee43756be149aac216cfa2794ba28f66137962"
-  :revdesc "0dee43756be1"
+  :commit "2dc48e5fb9c37390d9290d4f5ab371c39b7a3829"
+  :revdesc "2dc48e5fb9c3"
   :keywords '("lisp")
   :authors '(("Steve Purcell" . "steve@sanityinc.com")
              ("Fanael Linithien" . "fanael4@gmail.com"))
