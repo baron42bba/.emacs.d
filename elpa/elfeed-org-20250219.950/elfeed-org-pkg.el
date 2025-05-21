a079c5c92382c203640be1f17a7cd8402541df2f
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-05-21 14:02:00 +0200

    update elfeed-org

--- elpa/elfeed-org-20250219.950/elfeed-org-pkg.el
+++ elpa/elfeed-org-20250219.950/elfeed-org-pkg.el
@@ -1,12 +1,12 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "elfeed-org" "20231009.1125"
+(define-package "elfeed-org" "20250219.950"
   "Configure elfeed with one or more org-mode files."
-  '((elfeed "1.1.1")
-    (org    "8.2.7")
-    (cl-lib "0.5"))
+  '((emacs  "28.1")
+    (elfeed "1.1.1")
+    (org    "8.2.7"))
   :url "https://github.com/remyhonig/elfeed-org"
-  :commit "fe59a96969bd321f5f9ec7317a4bc80943b94c86"
-  :revdesc "fe59a96969bd"
+  :commit "1197cf29f6604e572ec604874a8f50b58081176a"
+  :revdesc "1197cf29f660"
   :keywords '("news")
   :authors '(("Remy Honig" . "remyhonig@gmail.com"))
   :maintainers '(("Remy Honig" . "remyhonig@gmail.com")))
