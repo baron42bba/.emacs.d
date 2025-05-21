995e26fc8c182a8b80c64da54c6f9669f3ed65c9
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-05-21 15:01:36 +0200

    add mvn and maven-test-mode

---
+++ elpa/mvn-20181002.1617/mvn-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "mvn" "20181002.1617"
+  "Helpers for compiling with maven."
+  ()
+  :url "https://github.com/apgwoz/mvn-el"
+  :commit "ffa40235b7dabb6c6c165f64f32a963cde8031f0"
+  :revdesc "ffa40235b7da"
+  :keywords '("compilation" "maven" "java")
+  :authors '(("Andrew Gwozdziewycz" . "git@apgwoz.com"))
+  :maintainers '(("Andrew Gwozdziewycz" . "git@apgwoz.com")))
