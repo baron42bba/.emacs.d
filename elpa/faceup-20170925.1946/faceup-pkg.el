abef837cf0dbefd3dc0e7349dd11b2f580a7ff14
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-05-21 14:38:54 +0200

    install faceup

---
+++ elpa/faceup-20170925.1946/faceup-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "faceup" "20170925.1946"
+  "Markup language for faces and font-lock regression testing."
+  ()
+  :url "https://github.com/Lindydancer/faceup"
+  :commit "6c92dad56a133e14e7b27831e1bcf9b3a71ff154"
+  :revdesc "6c92dad56a13"
+  :keywords '("faces" "languages"))
