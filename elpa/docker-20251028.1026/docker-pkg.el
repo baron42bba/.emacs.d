af683ebe47dd64cb239fc62c4b853e600f68a501
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-11-25 19:31:03 +0100

    docker
    * bba.org: C-c d for docker and compile for Dockerfile.
    * elpa/docker-20251028.1026: docker app
    * elpa/docker-compose-mode-20200830.1336: Dockerfile support.

---
+++ elpa/docker-20251028.1026/docker-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "docker" "20251028.1026"
+  "Interface to Docker."
+  '((aio       "1.0")
+    (dash      "2.19.1")
+    (emacs     "28.1")
+    (s         "1.13.0")
+    (tablist   "1.1")
+    (transient "0.4.3"))
+  :url "https://github.com/Silex/docker.el"
+  :commit "375e0ed45bb1edc655d9ae2943a09864bec1fcba"
+  :revdesc "375e0ed45bb1"
+  :keywords '("filename" "convenience")
+  :authors '(("Philippe Vaucher" . "philippe.vaucher@gmail.com"))
+  :maintainers '(("Philippe Vaucher" . "philippe.vaucher@gmail.com")))
