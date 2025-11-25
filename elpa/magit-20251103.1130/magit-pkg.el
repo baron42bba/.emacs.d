a53715eff1e16c21e7493289031991ae0f17a1f3
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-11-25 19:35:56 +0100

    update magit

---
+++ elpa/magit-20251103.1130/magit-pkg.el
@@ -0,0 +1,19 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit" "20251103.1130"
+  "A Git porcelain inside Emacs."
+  '((emacs         "28.1")
+    (compat        "30.1")
+    (cond-let      "0.1")
+    (llama         "1.0")
+    (magit-section "4.4")
+    (seq           "2.24")
+    (transient     "0.10")
+    (with-editor   "3.4"))
+  :url "https://github.com/magit/magit"
+  :commit "84c5c8c8c6108f6a5dbae1f145518cbaa9ca7480"
+  :revdesc "84c5c8c8c610"
+  :keywords '("git" "tools" "vc")
+  :authors '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+             ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+                 ("Kyle Meyer" . "kyle@kyleam.com")))
---
+++ elpa/magit-section-20251027.1634/magit-section-pkg.el
@@ -0,0 +1,14 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit-section" "20251027.1634"
+  "Sections for read-only buffers."
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.1")
+    (llama    "1.0")
+    (seq      "2.24"))
+  :url "https://github.com/magit/magit"
+  :commit "c58fc97c0ae53647148a3b490765d0a02fcb00ce"
+  :revdesc "c58fc97c0ae5"
+  :keywords '("tools")
+  :authors '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")))
