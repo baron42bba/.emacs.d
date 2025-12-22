43024eee0fbb28a6c47411d1dc84fa17795242de
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-12-22 18:25:22 +0100

    update helm
    * bba.org: point to new version
    * elpa/helm-20251212.555, elpa/helm-core-20251211.709

---
+++ elpa/helm-20251212.555/helm-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm" "20251212.555"
+  "Helm is an Emacs incremental and narrowing framework."
+  '((helm-core "4.0.6")
+    (wfnames   "1.2"))
+  :url "https://emacs-helm.github.io/helm/"
+  :commit "08fb63fab625e90b699999151dada80232a7c81b"
+  :revdesc "08fb63fab625"
+  :keywords '("helm" "convenience" "files" "buffers" "grep" "completion" "lisp" "matching" "tools" "help")
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
---
+++ elpa/helm-core-20251211.709/helm-core-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm-core" "20251211.709"
+  "Development files for Helm."
+  '((emacs "25.1")
+    (async "1.9.9"))
+  :url "https://emacs-helm.github.io/helm/"
+  :commit "c9a6c2542e5ec48700d1a72632cef009e5ee3253"
+  :revdesc "c9a6c2542e5e"
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
