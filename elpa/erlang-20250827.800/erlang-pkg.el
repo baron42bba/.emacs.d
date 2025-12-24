72fafa730262edeba6b187fee863d442bee07e6b
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-12-24 14:36:22 +0100

    update erlang

--- elpa/erlang-20250827.800/erlang-pkg.el
+++ elpa/erlang-20250827.800/erlang-pkg.el
@@ -1,8 +1,8 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "erlang" "20250520.1119"
+(define-package "erlang" "20250827.800"
   "Major modes for editing and running Erlang."
   '((emacs "24.3"))
   :url "https://github.com/erlang/otp"
-  :commit "9e6f6742c4d9e9915ee8af0dcb7d97cf1f836116"
-  :revdesc "9e6f6742c4d9"
+  :commit "ed8445a63861f510d21e27c9122e4fafa8039d9d"
+  :revdesc "ed8445a63861"
   :keywords '("erlang" "languages" "processes"))
