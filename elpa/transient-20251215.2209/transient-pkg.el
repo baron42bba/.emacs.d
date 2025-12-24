f76ca2645bf9d116ac2b9ede349290438164db09
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-12-24 14:28:35 +0100

    update transient

--- elpa/transient-20251215.2209/transient-pkg.el
+++ elpa/transient-20251215.2209/transient-pkg.el
@@ -1,12 +1,13 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "transient" "20250520.1040"
+(define-package "transient" "20251215.2209"
   "Transient commands."
-  '((emacs  "26.1")
-    (compat "30.1.0.0")
-    (seq    "2.24"))
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.2")
+    (seq      "2.24"))
   :url "https://github.com/magit/transient"
-  :commit "cb6550d5b111b7815feec97b236ecb051de70dbe"
-  :revdesc "cb6550d5b111"
+  :commit "066b985b19afbcc8f2f344cebb8d2f4c626b1bdc"
+  :revdesc "066b985b19af"
   :keywords '("extensions")
   :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
