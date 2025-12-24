ea05849ceab4d88ea79a3fed1c8e0d2441901aeb
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-12-24 15:15:25 +0100

    add glab

---
+++ elpa/glab-20250620.1333/glab-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "glab" "20250620.1333"
+  "Client library for the Gitlab API."
+  '((ghub "4"))
+  :url "https://github.com/emacsattic/glab"
+  :commit "655c0cd042b8e4abe6d314846f4c55b146f9fdee"
+  :revdesc "655c0cd042b8"
+  :keywords '("tools")
+  :authors '(("Jonas Bernoulli" . "emacs.ghub@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.ghub@jonas.bernoulli.dev")))
