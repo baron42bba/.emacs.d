a216911d281d327c500c3e0e95b278e7d1ba2b18
Andreas Gerler <baron@bundesbrandschatzamt.de>
2025-12-24 13:58:07 +0100

    remove puppet-mode and reset flycheck-json-python-json-executable

--- elpa/puppet-mode-20210305.645/puppet-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; puppet-mode-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "puppet-mode" "puppet-mode.el" (0 0 0 0))
-;;; Generated autoloads from puppet-mode.el
-
-(autoload 'puppet-mode "puppet-mode" "\
-
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.pp\\'" . puppet-mode))
-
-(register-definition-prefixes "puppet-mode" '("puppet-"))
-
-;;;***
-
-;;;### (autoloads nil nil ("puppet-mode-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; puppet-mode-autoloads.el ends here
