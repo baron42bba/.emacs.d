b07f2b865c30b0ebaf956e906422b802baf1a8c6
Andreas Gerler <baron@bundesbrandschatzamt.de>
2026-01-29 22:22:14 +0100

    added gptel

---
+++ elpa/gptel-20260123.739/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20260123.739"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "96199593c107767ffe0b59ab2a1f996c112e4fdf"
+  :revdesc "96199593c107"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
